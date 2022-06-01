b0515c6e4227ed54f432fc13fe44738a628c4cb2
Peter Eriksson <peter.eriksson@havochvatten.se>
2022-06-01 14:19:25 +0200

    formatting

--- ra/src/main/java/fish/focus/uvms/ais/Sentence.java
+++ ra/src/main/java/fish/focus/uvms/ais/Sentence.java
@@ -7,32 +7,32 @@ import java.util.stream.Collectors;
 
 public class Sentence {
 
-	private String commentBlock;
-	private String payload;
+    private String commentBlock;
+    private String payload;
 
-	public Sentence(String commentBlock, String payload) {
-		this.commentBlock = commentBlock;
-		this.payload = payload;
-	}
+    public Sentence(String commentBlock, String payload) {
+        this.commentBlock = commentBlock;
+        this.payload = payload;
+    }
 
-	public String getSentence() {
-		return payload;
-	}
+    public String getSentence() {
+        return payload;
+    }
 
-	public boolean hasValidCommentBlock() {
-		if (commentBlock == null) {
-			return false;
-		}
-		String[] commentBlockAndChecksum = commentBlock.split("\\*");
+    public boolean hasValidCommentBlock() {
+        if (commentBlock == null) {
+            return false;
+        }
+        String[] commentBlockAndChecksum = commentBlock.split("\\*");
         int calculatedSum = commentBlockAndChecksum[0].chars().boxed().reduce(0, (a, b) -> a ^= b);
         return Integer.parseInt(commentBlockAndChecksum[1], 16) == calculatedSum;
-	}
+    }
 
-	public Instant getCommentBlockLesTimestamp() {
-		if (commentBlock == null) {
-			return null;
-		}
-		String[] commentBlockAndChecksum = commentBlock.split("\\*");
+    public Instant getCommentBlockLesTimestamp() {
+        if (commentBlock == null) {
+            return null;
+        }
+        String[] commentBlockAndChecksum = commentBlock.split("\\*");
         Map<String, String> commentBlockProperties = Arrays.asList(commentBlockAndChecksum[0].split(","))
                 .stream()
                 .map(pair -> pair.split(":"))
@@ -41,5 +41,5 @@ public class Sentence {
             return null;
         }
         return Instant.ofEpochSecond(Long.valueOf(commentBlockProperties.get("c")));
-	}
+    }
 }
